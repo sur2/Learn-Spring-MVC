95dfdf8080d1bf407ac90bc7bb44dd472ce5cfa6
JEONG PYO YONG <sur2@naver.com>
2021-06-26 16:23:17 +0900

    redirect

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,7 +37,7 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    /*@PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
                        @RequestParam int price,
                        @RequestParam Integer quantity,
@@ -46,27 +47,58 @@ public class BasicItemController {
         model.addAttribute("item", item);
 
         return "basic/item";
-    }*/
+    }
 
-    /*@PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item) {
         itemRepository.save(item);
 //        model.addAttribute("item", item); // ModelAttribute에서 처리
         return "basic/item";
-    }*/
+    }
 
-    /*@PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         itemRepository.save(item);
 //        model.addAttribute("item", item); // Item -> item인 상태로 ModelAttribute에 담김
         return "basic/item";
-    }*/
+    }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(Item item) {
         // ModelAttribute 어노테이션 마져 생략가능(클래스 명 첫 문자 소문자로 지정됨)
         itemRepository.save(item);
-        return "basic/item";
+        return "/basic/item";
+    }
+    
+    // 새로고침 시 POST가 다시 전송됨을 방지하기 위한 redirect 추가
+    //@PostMapping("/add")
+    public String addItemV5(Item item) {
+        // ModelAttribute 어노테이션 마져 생략가능(클래스 명 첫 문자 소문자로 지정됨)
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    // redirect 인코딩 문제를 해결하기 위해 RedirectAttribute를 추가, 쿼리 마라미터 추가
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        // ModelAttribute 어노테이션 마져 생략가능(클래스 명 첫 문자 소문자로 지정됨)
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findByItem(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
